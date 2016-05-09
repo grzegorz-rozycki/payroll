234af8f10f380d4e5a60e1d822d43c0abfbe90a1
Grzegorz Różycki <grozycki@op.pl>
2016-05-09 23:07:52 +0200

    updated AddSalariedEmployee class

--- src/pl/grzeniu/payroll/AddSalariedEmployee.java
+++ src/pl/grzeniu/payroll/AddSalariedEmployee.java
@@ -4,13 +4,22 @@ package pl.grzeniu.payroll;
  * Created by Grzegorz Różycki on 08.05.16
  */
 public class AddSalariedEmployee extends AddEmployee {
+    protected double salary = 0.0;
 
-    public AddSalariedEmployee(int id, String name, String address, float salary) {
 
+    public AddSalariedEmployee(int empId, String empName, String empAddress, double salary) {
+        super(empId, empName, empAddress);
+        this.salary = salary;
+    }
+
+
+    @Override
+    protected PaymentClassification makeClassification() {
+        return new SalariedClassification(salary);
     }
 
     @Override
-    public void execute() {
-
+    protected PaymentSchedule makeSchedule() {
+        return new MonthlySchedule();
     }
 }
