912ea5125773e6d47f233f69b91d40fe2c6a7278
Grzegorz Różycki <grozycki@op.pl>
2016-05-10 21:34:05 +0200

    Add test for DeleteEmployee transaction

---
+++ test/pl/grzeniu/payroll/DeleteEmployeeTest.java
@@ -0,0 +1,29 @@
+package pl.grzeniu.payroll;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+/**
+ * Created by Grzegorz Różycki on 10.05.16
+ */
+public class DeleteEmployeeTest {
+
+    @Test
+    public void executeTest() {
+        final int empId = 4;
+
+        final Transaction t = new AddCommissionedEmployee(empId, "Bill", "Home", 2500, 3.2);
+        t.execute();
+
+        Employee e = PayrollDatabase.getEmployee(empId);
+        assertNotNull("Employee should exists", e);
+
+        final Transaction dt = new DeleteEmployee(empId);
+        dt.execute();
+
+        e = PayrollDatabase.getEmployee(empId);
+        assertNull("Employee shouldn't exist now", e);
+    }
+}
