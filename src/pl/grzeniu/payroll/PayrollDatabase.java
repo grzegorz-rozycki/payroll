c770f047fb44ecd776599641f0dba52648a59270
Grzegorz Różycki <grozycki@op.pl>
2016-05-10 21:20:08 +0200

    Convert PayrollDatabase's inner container to a map

--- src/pl/grzeniu/payroll/PayrollDatabase.java
+++ src/pl/grzeniu/payroll/PayrollDatabase.java
@@ -1,17 +1,17 @@
 package pl.grzeniu.payroll;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Grzegorz Różycki on 09.05.16
  */
 public class PayrollDatabase {
-    protected static List<Employee> employees = new ArrayList<>();
+    protected static Map<Integer, Employee> employees = new HashMap<>();
 
 
     public static void addEmployee(int id, Employee emp) {
-        employees.add(id, emp);
+        employees.put(id, emp);
     }
 
     public static Employee getEmployee(int id) {
