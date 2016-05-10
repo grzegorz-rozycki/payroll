41676da66bffdcca2d546bb6aecb4d028a1575d8
Grzegorz Różycki <grozycki@op.pl>
2016-05-10 21:12:12 +0200

    updated Employee class

--- src/pl/grzeniu/payroll/Employee.java
+++ src/pl/grzeniu/payroll/Employee.java
@@ -4,4 +4,17 @@ package pl.grzeniu.payroll;
  * Created by Grzegorz Różycki on 09.05.16
  */
 public class Employee {
+    public int id;
+    public String name;
+    public String address;
+    public PaymentSchedule schedule;
+    public PaymentClassification classification;
+    public PaymentMethod method;
+
+
+    public Employee(int id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
 }
