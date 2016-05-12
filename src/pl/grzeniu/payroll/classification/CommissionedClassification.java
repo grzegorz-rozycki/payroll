0514827f1ae65f09b8e7886ce674b4acafca2e6f
Grzegorz Różycki <grozycki@op.pl>
2016-05-12 21:09:04 +0200

    fix spelling in field commissionRate

--- src/pl/grzeniu/payroll/classification/CommissionedClassification.java
+++ src/pl/grzeniu/payroll/classification/CommissionedClassification.java
@@ -1,15 +1,25 @@
 package pl.grzeniu.payroll.classification;
 
+import java.util.Date;
+
 /**
  * Created by Grzegorz Różycki on 10.05.16
  */
 public class CommissionedClassification extends PaymentClassification {
     public double salary = 0.0;
-    public double commisionRate = 0.0;
+    public double commissionRate = 0.0;
 
-    public CommissionedClassification(double salary, double commisionRate) {
+    public CommissionedClassification(double salary, double commissionRate) {
         super();
         this.salary = salary;
-        this.commisionRate = commisionRate;
+        this.commissionRate = commissionRate;
+    }
+
+    public void addSalesReceipt(SalesReceipt salesReceipt) {
+
+    }
+
+    public SalesReceipt getSalesReceipt(Date date) {
+        return null;
     }
 }
