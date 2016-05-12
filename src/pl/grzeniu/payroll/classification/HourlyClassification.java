524a122b43e464a0c2ab4fb4e2ea0de0d67185ec
Grzegorz Różycki <grozycki@op.pl>
2016-05-12 20:43:33 +0200

    HourlyClassification aggregates TimeCards

--- src/pl/grzeniu/payroll/classification/HourlyClassification.java
+++ src/pl/grzeniu/payroll/classification/HourlyClassification.java
@@ -1,12 +1,16 @@
 package pl.grzeniu.payroll.classification;
 
 import java.util.Date;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Created by Grzegorz Różycki on 10.05.16
  */
 public class HourlyClassification extends PaymentClassification {
+    protected Map<Long, TimeCard> timeCards = new TreeMap<>();
     public double hourlyRate = 0.0;
+
 
     public HourlyClassification(double hourlyRate) {
         super();
@@ -14,10 +18,10 @@ public class HourlyClassification extends PaymentClassification {
     }
 
     public void addTimeCard(TimeCard timeCard) {
-
+        timeCards.put(timeCard.date.getTime(), timeCard);
     }
 
     public TimeCard getTimeCard(Date date) {
-        return null;
+        return timeCards.get(date.getTime());
     }
 }
