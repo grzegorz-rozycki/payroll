b29f9401f8097e3e42f7b07be7ad3f400e4068f5
Grzegorz Różycki <grozycki@op.pl>
2016-05-30 21:24:05 +0200

    add a charge list to union affiliation

--- src/pl/grzeniu/payroll/UnionAffiliation.java
+++ src/pl/grzeniu/payroll/UnionAffiliation.java
@@ -1,13 +1,25 @@
 package pl.grzeniu.payroll;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Grzegorz Różycki on 17.05.16
  */
 public class UnionAffiliation {
+    protected Map<Date, ServiceCharge> chargeList = new HashMap<>();
+
+    public void addServiceCharge(Date date, ServiceCharge charge) {
+        assert date != null;
+        assert charge != null;
+
+        chargeList.put(date, charge);
+    }
 
     public ServiceCharge getServiceCharge(Date date) {
-        return null;
+        assert date != null;
+
+        return chargeList.get(date);
     }
 }
