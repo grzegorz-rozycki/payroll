385898a08e102ac3dadca22638543461371bf401
Grzegorz Różycki <grozycki@op.pl>
2016-05-31 19:06:58 +0200

    add date field and constructor to ServiceCharge class

--- src/pl/grzeniu/payroll/ServiceCharge.java
+++ src/pl/grzeniu/payroll/ServiceCharge.java
@@ -1,8 +1,17 @@
 package pl.grzeniu.payroll;
 
+import java.util.Date;
+
 /**
  * Created by Grzegorz Różycki on 17.05.16
  */
 public class ServiceCharge {
     public double amount = 0.0;
+    public Date date = null;
+
+    public ServiceCharge(double amount, Date date) {
+        this.amount = amount;
+        this.date = date;
+    }
+
 }
