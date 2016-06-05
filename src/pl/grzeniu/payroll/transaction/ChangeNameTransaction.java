b2846c839e1dfec7a9e5ffbd4dbef820ddd4ea41
Grzegorz Różycki <grozycki@op.pl>
2016-06-05 19:40:17 +0200

    Test for change name transaction
    with ChangeNameTransaction class stub

---
+++ src/pl/grzeniu/payroll/transaction/ChangeNameTransaction.java
@@ -0,0 +1,13 @@
+package pl.grzeniu.payroll.transaction;
+
+/**
+ * Created by Grzegorz Różycki on 05.06.16
+ */
+public class ChangeNameTransaction implements Transaction {
+
+
+    @Override
+    public void execute() {
+
+    }
+}
---
+++ test/pl/grzeniu/payroll/ChangeNameTransactionTest.java
@@ -0,0 +1,25 @@
+package pl.grzeniu.payroll;
+
+import org.junit.Test;
+import pl.grzeniu.payroll.transaction.AddHourlyEmployeeTransaction;
+import pl.grzeniu.payroll.transaction.ChangeNameTransaction;
+
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by Grzegorz Różycki on 05.06.16
+ */
+public class ChangeNameTransactionTest {
+
+    @Test
+    public void executeTest() {
+        final int empId = 2;
+        (new AddHourlyEmployeeTransaction(empId, "Bill", "Home", 15.25)).execute();
+        (new ChangeNameTransaction()).execute();
+        final Employee emp = PayrollDatabase.getEmployee(empId);
+
+        assertNotNull("Employee with given id should exist", emp);
+        assertEquals("Employee's name should be Bob", "Bob", emp.name);
+    }
+}
