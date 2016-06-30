be771930044cfdf621d44a3ca19f587a53dae91e
Grzegorz Różycki <grozycki@op.pl>
2016-06-30 16:10:55 +0200

    PaydayTransaction.execute implementation
    faithful to the book

--- src/pl/grzeniu/payroll/transaction/PaydayTransaction.java
+++ src/pl/grzeniu/payroll/transaction/PaydayTransaction.java
@@ -1,8 +1,12 @@
 package pl.grzeniu.payroll.transaction;
 
+import pl.grzeniu.payroll.Employee;
 import pl.grzeniu.payroll.Paycheck;
+import pl.grzeniu.payroll.PayrollDatabase;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Grzegorz Różycki on 26.06.16.
@@ -10,15 +14,25 @@ import java.util.Date;
 public class PaydayTransaction implements Transaction {
 
     protected final Date payDate;
-
+    protected final Map<Integer, Paycheck> paychecks;
 
     public PaydayTransaction(Date payDate) {
         this.payDate = payDate;
+        this.paychecks = new HashMap<>();
     }
 
     @Override
     public void execute() {
 
+        for (final int empId : PayrollDatabase.getAllEmployeeIds()) {
+            final Employee employee = PayrollDatabase.getEmployee(empId);
+
+            if (employee.isPayDate(payDate)) {
+                Paycheck pc = new Paycheck(payDate);
+                paychecks.put(empId, pc);
+                employee.payday(pc);
+            }
+        }
     }
 
     public Paycheck getPaycheck(final int employeeId) {
