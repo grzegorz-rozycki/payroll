451fd28357d68a568cc8903c1dd082bf3a9862cf
Grzegorz Różycki <grozycki@op.pl>
2016-06-30 16:09:27 +0200

    default implementation of isPayDate on PaymentSchedule

--- src/pl/grzeniu/payroll/schedule/PaymentSchedule.java
+++ src/pl/grzeniu/payroll/schedule/PaymentSchedule.java
@@ -1,7 +1,13 @@
 package pl.grzeniu.payroll.schedule;
 
+import java.util.Date;
+
 /**
  * Created by Grzegorz Różycki on 10.05.16
  */
 public class PaymentSchedule {
+
+    public boolean isPayDate(Date date) {
+        return false;
+    }
 }
