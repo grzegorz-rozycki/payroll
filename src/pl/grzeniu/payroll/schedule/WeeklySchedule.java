0dfddf2627786736f53611d56d217edf573ac6b0
Grzegorz Różycki <grozycki@op.pl>
2016-07-26 21:01:55 +0200

    override isPayDate on WeeklySchedule

--- src/pl/grzeniu/payroll/schedule/WeeklySchedule.java
+++ src/pl/grzeniu/payroll/schedule/WeeklySchedule.java
@@ -1,7 +1,13 @@
 package pl.grzeniu.payroll.schedule;
 
+import java.util.Date;
+
 /**
  * Created by Grzegorz Różycki on 10.05.16
  */
 public class WeeklySchedule extends PaymentSchedule {
+    @Override
+    public boolean isPayDate(Date date) {
+        return (date.getDay() == 5);
+    }
 }
