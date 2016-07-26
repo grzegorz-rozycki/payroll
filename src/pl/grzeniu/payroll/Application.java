d1a3f31ce3ddd4a2e913b9f4deceece79437b33f
Grzegorz Różycki <grozycki@op.pl>
2016-07-26 21:02:42 +0200

    main application class

---
+++ src/pl/grzeniu/payroll/Application.java
@@ -0,0 +1,13 @@
+package pl.grzeniu.payroll;
+
+import java.util.Date;
+
+/**
+ * Created by Grzegorz Różycki on 26.07.16
+ */
+public class Application {
+
+    public static void main(String[] argv) {
+        System.out.println("done...");
+    }
+}
