511ff91de5ad41b97e9eb3b4ce8df283b1721237
Grzegorz Różycki <grozycki@op.pl>
2016-11-08 21:23:00 +0100

    UnionAffiliation.calculateDues method implementation

--- src/pl/grzeniu/payroll/UnionAffiliation.java
+++ src/pl/grzeniu/payroll/UnionAffiliation.java
@@ -1,5 +1,7 @@
 package pl.grzeniu.payroll;
 
+import java.time.Period;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,7 +19,7 @@ public class UnionAffiliation extends Affiliation {
     }
 
     public UnionAffiliation() {
-        super();
+        this(0.0);
     }
 
     public void addServiceCharge(Date date, double charge) {
@@ -29,5 +31,26 @@ public class UnionAffiliation extends Affiliation {
         assert date != null;
 
         return chargeList.get(date);
+    }
+
+    @Override
+    public double calculateDeductions(Paycheck paycheck) {
+        return (dues * numberOfFridaysInPayPeriod(paycheck.payPeriodStart, paycheck.payPeriodEnd));
+    }
+
+    private int numberOfFridaysInPayPeriod(Date payPeriodStart, Date payPeriodEnd) {
+        final Calendar cal = Calendar.getInstance();
+        final int days = (int)(payPeriodEnd.getTime() - payPeriodStart.getTime()) / 86400000;
+        int fridays = 0;
+
+        cal.setTime(payPeriodEnd);
+
+        for (int i = 0; i < days; i += 1, cal.add(Calendar.DATE, -1)) {
+            if (Calendar.FRIDAY == cal.get(Calendar.DAY_OF_WEEK)) {
+                fridays += 1;
+            }
+        }
+
+        return fridays;
     }
 }
